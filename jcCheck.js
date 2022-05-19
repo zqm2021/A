f2a9d7cfba0c2a1719ba519145a1917ca867dc30
zqm2021 <zqm2021@protonmail.com>
2022-05-19 19:39:28 +0800

    sync

--- jcCheck.js
+++ jcCheck.js
@@ -1,45 +1,44 @@
 /*
 机场签到 需要在脚本里填写邮箱密码
 cron "0 0 8 * * ?" ql_jcCheck.js
-https://raw.githubusercontent.com/QiFengg/QuantumultX_Conf/main/Scripts/jcCheck.js
 */
+
+const $ = new Env("机场签到");
+const notify = $.isNode() ? require("./sendNotify") : "";
 
 /*
 1.好家伙机场
 付费 便宜3.99 50g  流量每隔31天重置 签到每天1g-4g随机 可看NF 速度快 提供小火箭下载账号
-注册地址:haojiahuo.live/auth/register?code=FErc
+注册地址:haojiahuo.live
 */
 let hjh = {
-    email: "",
-    pwd: "",
+    email: $.isNode() ? process.env.hjhEmail : '',
+    pwd: $.isNode() ? process.env.hjhPassword : ''
 };
 
 /*
 2.ikuuu机场
-注册地址:https://ikuuu.co/auth/register?code=Hg2Y
+注册地址:https://ikuuu.co/
 白嫖版 注册送50g 签到每天1g以内 随机 速度还行
 套餐10元300g 提供小火箭下载账号
 */
 
 let iku = {
-    email: "",
-    pwd: "",
+    email: $.isNode() ? process.env.ikuuuEmail : '',
+    pwd: $.isNode() ? process.env.ikuuuPassword : ''
 };
 
 /*
 3.几鸡机场
-注册地址:https://b.luxury/waf/GGWE9QhLsLE74X422
+注册地址:https://b.luxury/
 白嫖版 注册送10g 签到每天1g-4g随机 速度还行
 付费很多套餐 5块钱一个月300G 流量每隔31天重置 控制套餐购买人数
 */
 
 let jj = {
-    email: "",
-    pwd: "",
+    email: $.isNode() ? process.env.luxuryEmail : '',
+    pwd: $.isNode() ? process.env.luxuryPassword : ''
 };
-
-const $ = new Env("机场签到");
-const notify = $.isNode() ? require("./sendNotify") : "";
 
 !(async () => {
     console.log(
